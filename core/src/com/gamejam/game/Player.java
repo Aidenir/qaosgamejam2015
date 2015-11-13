66714587977aea25a736279b0e21a35e03c0df6f
Viktor Hansson <viktor.hansson@me.com>
2015-11-13 22:20:13 +0100

    Added swiping gestures and jump

--- core/src/com/gamejam/game/Player.java
+++ core/src/com/gamejam/game/Player.java
@@ -3,6 +3,9 @@ package com.gamejam.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.input.GestureDetector;
+import com.badlogic.gdx.input.GestureDetector.GestureListener;
+import com.badlogic.gdx.math.Vector2;
 
 public class Player {
 	
@@ -13,6 +16,12 @@ public class Player {
 	private Texture playerImg;
 	private int playerImgWidth;
 	private int playerImgHeight;
+
+	private boolean isJumping;
+	private int jumpSpeed = 600;
+	private int jumpHeight = 300;
+
+	private boolean goingDown;
 	
 	public Player(GameJam game){
 		this.game = game;
@@ -21,10 +30,31 @@ public class Player {
 		playerSprite = new Sprite(playerImg, 76,136);
 		playerSprite.setX(50);
 		playerSprite.setY(50);
+		isJumping = false;
+		
+		Gdx.input.setInputProcessor(new GestureDetector(new SwipeGestureHandler()));
+
 	}
 	
 	public void update(float delta){
 		this.handleInput();
+		
+		if(isJumping){
+			if(playerSprite.getY() > jumpHeight){
+				this.goingDown = true;
+			}
+			else if(playerSprite.getY() <= 50){
+				isJumping = false;
+				goingDown = false;
+			}
+			
+			if(goingDown){
+				playerSprite.setY(playerSprite.getY() - jumpSpeed * delta);
+				if(playerSprite.getY() < 50 ) playerSprite.setY(50);
+			}else{
+				playerSprite.setY(playerSprite.getY() + jumpSpeed * delta);
+			}
+		}
 		
 		game.batch.begin();
 		playerSprite.draw(game.batch);
@@ -35,7 +65,89 @@ public class Player {
 		
 	}
 	
+	public void jump(){
+		if(isJumping) return;
+		this.isJumping = true;
+		this.goingDown = false;
+	}
+	
+	public void swipeUp(){
+		jump();
+	}
+	public void swipeDown(){
+		
+	}
+	public void swipeRight(){
+		
+	}
+	public void swipeLeft(){
+		
+	}
 	public void dispose(){
 		
+	}
+	
+	public class SwipeGestureHandler implements GestureListener{
+
+		@Override
+		public boolean touchDown(float x, float y, int pointer, int button) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean tap(float x, float y, int count, int button) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean longPress(float x, float y) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean fling(float velocityX, float velocityY, int button) {
+			//Swipe up
+			if(Math.abs(velocityX) < Math.abs(velocityY)){
+				if(velocityY > 0 ){
+					swipeDown();
+				}else{
+					swipeUp();
+				}
+			}else{
+				if(velocityX > 0 ){
+					swipeRight();
+				}else{
+					swipeLeft();
+				}
+			}
+			return true;
+		}
+
+		@Override
+		public boolean pan(float x, float y, float deltaX, float deltaY) {
+			return false;
+		}
+
+		@Override
+		public boolean panStop(float x, float y, int pointer, int button) {
+			return false;
+		}
+
+		@Override
+		public boolean zoom(float initialDistance, float distance) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+		@Override
+		public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+			// TODO Auto-generated method stub
+			return false;
+		}
+
+
 	}
 }
